1d0faeb5e925dd7c9e71f0f060a5d30022d3d75a
Anton Bashirov <anton.sh.local@gmail.com>
2025-03-06 13:33:59 +0400

    fix memory limit (#7)

--- caddy/mercure/main.go
+++ caddy/mercure/main.go
@@ -1,8 +1,13 @@
-// Copied from https://github.com/caddyserver/xcaddy/blob/b7fd102f41e12be4735dc77b0391823989812ce8/environment.go#L251
 package main
 
 import (
+	"log/slog"
+
+	"github.com/caddyserver/caddy/v2"
+
+	"github.com/KimMachineGun/automemlimit/memlimit"
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	"go.uber.org/zap/exp/zapslog"
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
@@ -10,5 +15,19 @@ import (
 )
 
 func main() {
+	// Backport of https://github.com/caddyserver/caddy/pull/6809
+	// TODO: remove this block when Caddy 2.10 will be released
+	_, _ = memlimit.SetGoMemLimitWithOpts(
+		memlimit.WithLogger(
+			slog.New(zapslog.NewHandler(caddy.Log().Core())),
+		),
+		memlimit.WithProvider(
+			memlimit.ApplyFallback(
+				memlimit.FromCgroup,
+				memlimit.FromSystem,
+			),
+		),
+	)
+
 	caddycmd.Main()
 }
